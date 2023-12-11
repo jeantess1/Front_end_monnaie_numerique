4bf775127353c2028440d8a8efee694a761621ff
jtsd <teyssedrej@gmail.com>
2023-12-11 11:31:41 +0100

    up

--- src/components/All_votes.tsx
+++ src/components/All_votes.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { type Address, useContractRead } from 'wagmi';
 
 import { wagmiContractConfig } from './contracts';
 
-// Créez une fonction de hook personnalisée pour récupérer les données de vote
-function useVoteData(voteNumber) {
+// Renommez le hook personnalisé pour éviter les conflits
+function useVoteDataHook(voteNumber) {
   return useContractRead({
     ...wagmiContractConfig,
     functionName: 'votes',
@@ -16,7 +16,7 @@ export function All_votes() {
   const [voteNumbers, setVoteNumbers] = useState([]);
   const [allVotes, setAllVotes] = useState([]);
 
-  // Utilisez la fonction de hook personnalisée pour récupérer les données de vote
+  // Utilisez le hook personnalisé renommé pour récupérer les données de vote
   useEffect(() => {
     const fetchAllVotes = async () => {
       // Vous devrez adapter la fonction contractuelle pour récupérer la liste des votes
@@ -29,7 +29,7 @@ export function All_votes() {
       // Récupérer tous les votes au chargement initial
       const allVotesData = await Promise.all(
         voteNumbersArray.map(async (voteNumber) => {
-          const voteData = await useVoteData(voteNumber);
+          const voteData = await useVoteDataHook(voteNumber); // Utilisez le hook renommé ici
           return voteData?.toString() || '';
         })
       );
